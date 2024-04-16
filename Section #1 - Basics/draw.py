9de25404bef44bc55d07ae393d5e2a77a6d2c0d6
jackyhuuuu <166289337+jackyhuuuu@users.noreply.github.com>
2024-04-16 15:45:23 +0200

    Update draw.py

--- Section #1 - Basics/draw.py
+++ Section #1 - Basics/draw.py
@@ -3,15 +3,19 @@
 import cv2 as cv
 import numpy as np
 
-blank = np.zeros((500,500,3), dtype='uint8')
+# draw a blank image
+blank = np.zeros((500,500,3), dtype='uint8') # 3 is color channels
 cv.imshow('Blank', blank)
 
 # 1. Paint the image a certain colour
-blank[200:300, 300:400] = 0,0,255
+# blank[:] = 0,0,255 # paint a certain color in the whole image
+blank[200:300, 300:400] = 0,0,255 # paint a certain color in a certain portion of image 
 cv.imshow('Green', blank)
 
 # 2. Draw a Rectangle
-cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1)
+cv.rectangle(blank, (0,0), (blank.shape[1]//2, blank.shape[0]//2), (0,255,0), thickness=-1) 
+# (0,0) is the origin of rectangle
+# thickness = -1 means fullfill the rectangle with color
 cv.imshow('Rectangle', blank)
 
 # 3. Draw A circle
@@ -23,7 +27,7 @@ cv.line(blank, (100,250), (300,400), (255,255,255), thickness=3)
 cv.imshow('Line', blank)
 
 # 5. Write text
-cv.putText(blank, 'Hello, my name is Jason!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
+cv.putText(blank, 'Hello, my name is Jacky!!!', (0,225), cv.FONT_HERSHEY_TRIPLEX, 1.0, (0,255,0), 2)
 cv.imshow('Text', blank)
 
 cv.waitKey(0)
