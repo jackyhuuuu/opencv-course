99457bb584498a2c4fea7b12bbd055fa398f24e1
jackyhuuuu <166289337+jackyhuuuu@users.noreply.github.com>
2024-04-16 15:31:18 +0200

    Update rescale_resize.py

--- Section #2 - Advanced/rescale_resize.py
+++ Section #2 - Advanced/rescale_resize.py
@@ -5,19 +5,22 @@ import cv2 as cv
 # img = cv.imread('../Resources/Photos/cat.jpg')
 # cv.imshow('Cat', img)
 
-def rescaleFrame(frame, scale=0.75):
-    # Images, Videos and Live Video
+def rescaleFrame(frame, scale = 0.75): # set the dafault scale to 0.75
+    # works on Images, Videos and Live Video
     width = int(frame.shape[1] * scale)
     height = int(frame.shape[0] * scale)
 
-    dimensions = (width,height)
+    dimensions = (width, height)
 
-    return cv.resize(frame, dimensions, interpolation=cv.INTER_AREA)
+    return cv.resize(frame, dimensions, interpolation = cv.INTER_AREA)
 
-def changeRes(width,height):
-    # Live video
-    capture.set(3,width)
-    capture.set(4,height)
+def changeRes(width, height): # change the resolution
+    # only works on Live video
+    capture.set(3, width)
+    capture.set(4, height)
+    # 函数的一般形式是 capture.set(property, value)，其中 property 是要设置的属性的标识符，value 是要设置的属性值
+    # 对于capture.set()函数来说，参数3和参数4分别表示视频帧的宽度和高度
+    # e.g. changeRes(640, 480)表示将视频的分辨率设置为640x480
     
 # Reading Videos
 capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
@@ -25,12 +28,12 @@ capture = cv.VideoCapture('../Resources/Videos/dog.mp4')
 while True:
     isTrue, frame = capture.read()
 
-    frame_resized = rescaleFrame(frame, scale=.2)
+    frame_resized = rescaleFrame(frame, scale = .2)
     
     cv.imshow('Video', frame)
     cv.imshow('Video Resized', frame_resized)
 
-    if cv.waitKey(20) & 0xFF==ord('d'):
+    if cv.waitKey(20) & 0xFF == ord('d'):
         break
 
 capture.release()
