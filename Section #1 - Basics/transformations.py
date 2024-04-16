c710e2f2072d65bab4f1da5b0de74c633f92f462
jackyhuuuu <166289337+jackyhuuuu@users.noreply.github.com>
2024-04-16 16:27:50 +0200

    Update transformations.py

--- Section #1 - Basics/transformations.py
+++ Section #1 - Basics/transformations.py
@@ -8,12 +8,14 @@ cv.imshow('Park', img)
 
 # Translation
 def translate(img, x, y):
-    transMat = np.float32([[1,0,x],[0,1,y]])
+    transMat = np.float32([[1,0,x],[0,1,y]]) 
+    # 通过 np.float32() 创建了一个2x3的变换矩阵 transMat，用来描述平移变换
+    # 这个矩阵的第一行[1, 0, x]表示在x轴上的平移量，第二行[0, 1, y]表示在y轴上的平移量
     dimensions = (img.shape[1], img.shape[0])
-    return cv.warpAffine(img, transMat, dimensions)
+    return cv.warpAffine(img, transMat, dimensions) # 调用cv.warpAffine()函数, 该函数用于对图像进行仿射变换
 
 # -x --> Left
-# -y --> Up
+# -y --> Up   
 # x --> Right
 # y --> Down
 
@@ -28,6 +30,7 @@ def rotate(img, angle, rotPoint=None):
         rotPoint = (width//2,height//2)
     
     rotMat = cv.getRotationMatrix2D(rotPoint, angle, 1.0)
+    # cv.getRotationMatrix2D()函数创建一个旋转矩阵rotMat, 接受三个参数：旋转中心点rotPoint、旋转角度angle和缩放比例1.0
     dimensions = (width,height)
 
     return cv.warpAffine(img, rotMat, dimensions)
@@ -43,12 +46,13 @@ resized = cv.resize(img, (500,500), interpolation=cv.INTER_CUBIC)
 cv.imshow('Resized', resized)
 
 # Flipping
-flip = cv.flip(img, -1)
+# flip = cv.flip(img, 1) # flip horizontally
+# flip = cv.flip(img, 0) # flip vertically
+flip = cv.flip(img, -1) # flip both horizontally and vertically
 cv.imshow('Flip', flip)
 
 # Cropping
 cropped = img[200:400, 300:400]
 cv.imshow('Cropped', cropped)
-
 
 cv.waitKey(0)
